1787dd86b7db8b446a251fbaf7e9e4e8dac23848
misiom94 <misiom94@gmail.com>
2018-02-19 14:05:54 +0100

    dodawanie/usuwanie z listy

--- Losowanko/app/src/main/java/com/example/misio/losowanko/ActivityLosowanie.java
+++ Losowanko/app/src/main/java/com/example/misio/losowanko/ActivityLosowanie.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
@@ -16,10 +15,11 @@ import java.util.ArrayList;
 
 public class ActivityLosowanie extends AppCompatActivity {
 
-    Button buttonDodaj;
+    Button buttonDodajOsobe;
     TableLayout tableOsoby;
     EditText editTextOsoba;
     ArrayList<String> listaOsob;
+    ArrayList<String> listaZadan;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +27,20 @@ public class ActivityLosowanie extends AppCompatActivity {
         setContentView(R.layout.activity_losowanie);
 
 
-        buttonDodaj = (Button)findViewById(R.id.buttonDodaj);
+        buttonDodajOsobe = (Button)findViewById(R.id.buttonDodaj);
         tableOsoby = (TableLayout)findViewById(R.id.tableOsoby);
         editTextOsoba = (EditText)findViewById(R.id.editTextOsoba);
         listaOsob = new ArrayList<String>();
-
-        buttonDodaj.setOnClickListener(new View.OnClickListener() {
+        listaZadan = new ArrayList<String>();
+        buttonDodajOsobe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(checkField(editTextOsoba)){
-                    addOsoba(editTextOsoba);
+                    addPosition(editTextOsoba);
                     refreshTable();
                 }
             }
         });
-
-
     }
 
     public boolean checkField(TextView textView){
@@ -53,21 +51,37 @@ public class ActivityLosowanie extends AppCompatActivity {
         return true;
     }
 
-    public void addOsoba(EditText editText){
-        Log.e("DODAJ OSOBE: ",String.valueOf(editText.getText()));
+    public void addPosition(EditText editText){
+        Log.d("DODAJ OSOBE: ",String.valueOf(editText.getText()));
         String osoba = String.valueOf(editText.getText());
         listaOsob.add(osoba);
     }
 
+    public void removePosition(int i){
+        Log.d("USUWANIE","USUNIETO " + String.valueOf(listaOsob.get(i)));
+        listaOsob.remove(i);
+        refreshTable();
+    }
+
     public void refreshTable(){
         tableOsoby.removeAllViews();
-        for (int i=0; i<=listaOsob.size();i++){
+        for (int i=0; i<listaOsob.size();i++){
             TableRow rowOsoba = new TableRow(this);
+            Button buttonDelete = new Button(this);
+            buttonDelete.setText("Usun");
             rowOsoba.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
             TextView textView = new TextView(this);
             textView.setText(listaOsob.get(i));
             rowOsoba.addView(textView);
+            rowOsoba.addView(buttonDelete);
             tableOsoby.addView(rowOsoba);
+            final int finalI = i;
+            buttonDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    removePosition(finalI);
+                }
+            });
         }
     }
 }
